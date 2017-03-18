ec8c7f81e888a58d74f13a97c94a9fbb276a145b
Matej Vrzala <vrzala.matej.com>
2017-03-18 16:30:10 +0100

    security

--- app/config/database.php
+++ app/config/database.php
@@ -3,11 +3,11 @@ namespace app\config;
 
 class Database
 {
-    const HOST = 'localhost';
-    const NAME = 'slovakvegan_new';
-    const USER = 'phpmyadmin';
-    const PASSWORD = '123456';
+    const HOST = '';
+    const NAME = '';
+    const USER = '';
+    const PASSWORD = '';
     const SHOW_ERRORS = true;
 
-    const SALT = 'asd57rgre574sdfs5gs658floinbc489sdg49';
+    const SALT = '';
 }
