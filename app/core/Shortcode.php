7135ca9326b7d2e724130861d5282f29d01f750b
Matej Vrzala <vrzala.matej@gmail.com>
2017-11-29 15:23:32 +0100

    select from gallery

--- app/core/Shortcode.php
+++ app/core/Shortcode.php
@@ -13,14 +13,24 @@ class Shortcode
     //Instantiate a Shortcode Manager
     $this->manager = new Manager\ShortcodeManager;
     //Create your shortcode
-    $this->list = new Library\SimpleShortcode('shortcode', null, function(){
-        return 'hello this is gallery';
+    $this->list = new Library\SimpleShortcode('gallery', null, function(){
+        $items = array_diff(scandir(UPLOADS.DS.'pages'.DS.'29'), ['.', '..']);
+        $html = '<div class="row text-center text-lg-left">';
+        foreach ($items as $item) {
+          $html .= '<div class="col-lg-3 col-md-4 col-xs-6">';
+          $html .= '<a href="#" class="d-block mb-4 h-100">';
+          $html .= '<img class="img-fluid img-thumbnail" src="/public/uploads/pages/29/'.$item.'">';
+          $html .= '</a></div>';
+        }
+        $html .= '</div>';
+        return $html;
     });
 
   }
   public function handle ($content) 
   {
-    return $this->manager->register($this->list)->doShortcode($content); 
+    return $this->manager->register($this->list)
+                         ->doShortcode($content); 
   }
 
 }
